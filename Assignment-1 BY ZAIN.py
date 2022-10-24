d2c1316f28734c12bb50a73ed0801393b105a027
Muhammad Zain <114310149+Muhammad-Zain-Yaseen@users.noreply.github.com>
2022-10-24 16:37:45 +0500

    Update Assignment-1 BY ZAIN.py

--- Assignment-1 BY ZAIN.py
+++ Assignment-1 BY ZAIN.py
@@ -30,14 +30,14 @@ s= dfp.CGPA
 print(s)
 
 #Print the total number of students who have a three words name (first-middle-surname)
-df[df['Name'].str.split(' ').str.len() == 3]['Name'].count()
+dfp[dfp['Name'].str.split(' ').str.len() == 3]['Name'].count()
 
 
 #Those whose CGPA is more than pr equal to 3.0:
-#dfp = dfp[(dfp['CGPA'] >= 3)]
-#print(dfp.CGPA)
-#print(dfp.CGPA.count())
-#print(dfp.CGPA.count()/100)
+dfp = dfp[(dfp['CGPA'] >= 3)]
+print(dfp.CGPA)
+print(dfp.CGPA.count())
+print(dfp.CGPA.count()/100)
 
 #Plot the pie chart to show the ratio of Female and Male Students:
 print(dfp.Gender)
@@ -52,25 +52,25 @@ print(b)
 male= ['Male', 'female']
 female= ['females']
 numbers = [a, b]
-plt.pie(numbers, labels= male)
+plt.pie(numbers, labels= male,autopct='%1.1f%%')
 plt.show()
 
     
 #CGPA of all male students on a histogram with intervals 2.0-2.5, 2.6-3.0, 3.1-3.5, 3.6-4.0
-df = df.astype({'CGPA': 'float'})
-zain = df['CGPA']
+dfp = dfp.astype({'CGPA': 'float'})
+zain = dfp['CGPA']
 bins = [2.0,2.5,3.0,3.5,4.0]
 
 plt.hist(zain, bins=bins, edgecolor='blue')
 plt.show()
 
 #Plot the favorite colors of male vs female students on a bar chart
-(df['FavoriteColor'].str.strip().str).lower().value_counts().plot(kind='bar')
+(dfp['FavoriteColor'].str.strip().str).lower().value_counts().plot(kind='bar')
 
 
 #Creating the correlation matrix between HSSC-1 and HSSC-2 marks and then plot on a heatmap chart:
-df = df.astype({'HSSC-1': 'int','HSSC-2': 'int'})
-correlational = df[['HSSC-1','HSSC-2']].corr()
+dfp = dfp.astype({'HSSC-1': 'int','HSSC-2': 'int'})
+correlational = dfp[['HSSC-1','HSSC-2']].correlational()
 sns.heatmap(correlational, annot=True)
 plt.show()
 
